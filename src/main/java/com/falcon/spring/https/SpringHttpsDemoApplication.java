f559184dc372ada37b4961903402fe701c104604
Piyush Bel <piyushbelsare99@gmail.com>
2023-09-10 17:20:14 +0530

    forcefully build fail

--- src/main/java/com/falcon/spring/https/SpringHttpsDemoApplication.java
+++ src/main/java/com/falcon/spring/https/SpringHttpsDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringHttpsDemoApplication {
 
 	@GetMapping("/getData")
 	public String getMessage(){
-		return "Accessed by https request";
+		return "Accessed by https req"
 	}
 
 	public static void main(String[] args) {
